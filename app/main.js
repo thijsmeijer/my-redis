fb9468312b761bf8c60e4519fdd4368d883e384c
Thijs Meijer <t.meijer@flexpedia.nl>
2024-10-29 16:55:45 +0100

    add support for SET and GET commands

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const net = require('net');
 
+const store = {};
+
 const server = net.createServer((connection) => {
     console.log('Client connected');
 
@@ -7,17 +9,29 @@ const server = net.createServer((connection) => {
         const commands = data.toString().trim().split('\r\n');
 
         const command = commands[2];
-        let argument = '';
-
-        if (command.length > 2) {
-            argument = commands[4];
-        }
 
         if (command === 'ECHO') {
+            const argument = commands[4];
+
             const response = `$${argument.length}\r\n${argument}\r\n`;
             connection.write(response);
         } else if (command === 'PING') {
             connection.write('+PONG\r\n');
+        } else if (command === 'SET') {
+            const key = commands[4];
+
+            store[key] = commands[6];
+            connection.write('+OK\r\n');
+        } else if (command === 'GET') {
+            const key = commands[4];
+            const value = store[key];
+
+            if (value !== undefined) {
+                const response = `$${value.length}\r\n${value}\r\n`;
+                connection.write(response);
+            } else {
+                connection.write('$-1\r\n');
+            }
         } else {
             connection.write('-ERR unknown command\r\n');
         }
