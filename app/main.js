c40e11adbee01eb23853e2adf738804290da9ca0
Thijs Meijer <t.meijer@flexpedia.nl>
2024-10-29 16:09:42 +0100

    add support for multiple commands, for now echo and ping

--- app/main.js
+++ app/main.js
@@ -4,13 +4,31 @@ const server = net.createServer((connection) => {
     console.log('Client connected');
 
     connection.on('data', (data) => {
-        console.log(`Received data: ${data}`);
+        const commands = data.toString().trim().split('\r\n');
 
-        connection.write('+PONG\r\n');
+        const command = commands[2];
+        let argument = '';
+
+        if (command.length > 2) {
+            argument = commands[4];
+        }
+
+        if (command === 'ECHO') {
+            const response = `$${argument.length}\r\n${argument}\r\n`;
+            connection.write(response);
+        } else if (command === 'PING') {
+            connection.write('+PONG\r\n');
+        } else {
+            connection.write('-ERR unknown command\r\n');
+        }
     });
 
     connection.on('end', () => {
         console.log('Client disconnected');
+    });
+
+    connection.on('error', (err) => {
+        console.error('Connection error:', err.message);
     });
 });
 
