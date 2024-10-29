b47146946b7bcc6dcb45ca6a1646d9b9cd1f413d
Thijs Meijer <t.meijer@flexpedia.nl>
2024-10-29 15:24:38 +0100

    add first hardcoded command

--- app/main.js
+++ app/main.js
@@ -1,5 +1,19 @@
 const net = require('net');
 
-const server = net.createServer((connection) => {});
+const server = net.createServer((connection) => {
+    console.log('Client connected');
 
-server.listen(6379, '127.0.0.1');
+    connection.on('data', (data) => {
+        console.log(`Received data: ${data}`);
+
+        connection.write('+PONG\r\n');
+    });
+
+    connection.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(6379, '127.0.0.1', () => {
+    console.log('Server listening on port 6379');
+});
